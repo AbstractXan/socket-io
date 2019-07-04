5205c5b54a283ddcdb9d4b7e3ddd04d6a8f60ea5
Priyansh Sangule <priyansh0506@gmail.com>
2019-07-04 13:04:16 +0530

    Heroku changes

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');  
 });
@@ -21,6 +26,6 @@ io.on('connection', function(socket){
     })
 });
   
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(port, function(){
+    console.log('listening on *:',port);
 });
